72e32f78aa40ac90efaf95c2c96d0371546c46a7
VanTruongNguyen1 <truongbttm@gmail.com>
2025-01-08 22:38:38 +0700

    git commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,20 @@ import * as bootstrap from "bootstrap"
 
 import "trix"
 import "@rails/actiontext"
+-- a/app/javascript/application.js
+++ b/app/javascript/application.js
+import "controllers"
+import "./controllers";
+
+-- a/app/javascript/controllers/index.js
+++ b/app/javascript/controllers/index.js
+import { application } from "controllers/application"
+
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
+
+// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
+// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
+// lazyLoadControllersFrom("controllers", application)
+import { application } from "./application";
